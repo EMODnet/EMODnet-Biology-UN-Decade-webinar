1802c8a746d68d73b67443e01c065c299cae8404
salvafern <salvador.fernandez@vliz.be>
2024-12-11 16:04:33 +0100

    ci: force dependency on eblondel/ows4R@1b317679e11e2acd106fc58b09a694ff19a6ae79

--- install.R
+++ install.R
@@ -19,4 +19,8 @@ install.packages("rerddap")
 
 # Retrieve Biological Occurrence Data
 install.packages("pak")
-pak::pkg_install("EMODnet/emodnet.wfs@d3c1ad2d40609fc4f64c80fb96bc20ce15d70594")
+install.packages("devtools")
+
+# Need specifically this version to work with emodnet.wfs
+pak::pkg_install("eblondel/ows4R@1b317679e11e2acd106fc58b09a694ff19a6ae79") 
+pak::pkg_install("EMODnet/emodnet.wfs@d3c1ad2d40609fc4f64c80fb96bc20ce15d70594", dependencies = FALSE)
